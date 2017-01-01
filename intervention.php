4809542870e308cbff6ac19082f1f959141c29d0
Darren Jacoby <darren@jacoby.co.za>
2017-01-01 15:00:40 +0200

    Update Meta 1.1.0

--- intervention.php
+++ intervention.php
@@ -3,7 +3,7 @@
 Plugin Name:        Intervention
 Plugin URI:         http://github.com/soberwp/intervention
 Description:        Lightweight WordPress plugin containing modules to cleanup and customize wp-admin easily.
-Version:            1.0.2
+Version:            1.1.0
 Author:             Sober
 Author URI:         http://github.com/soberwp/
 License:            MIT License
