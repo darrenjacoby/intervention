04f421b38c65477ba4ee4ca0a36f210439793c74
Darren Jacoby <darren@jacoby.co.za>
2017-08-26 13:10:57 +0200

    Composer package

--- intervention.php
+++ intervention.php
@@ -1,26 +1,6 @@
 <?php
-/*
-Plugin Name:        Intervention
-Plugin URI:         http://github.com/soberwp/intervention
-Description:        WordPress plugin containing modules to cleanup and customize wp-admin
-Version:            1.1.1
-Author:             Sober
-Author URI:         http://github.com/soberwp/
-License:            MIT License
-License URI:        http://opensource.org/licenses/MIT
-GitHub Plugin URI:  soberwp/intervention
-GitHub Branch:      master
-*/
+
 namespace Sober\Intervention;
-
-/**
- * Plugin
- */
-if (!defined('ABSPATH')) {
-    die;
-};
-
-require(file_exists($composer = __DIR__ . '/vendor/autoload.php') ? $composer : __DIR__ . '/dist/autoload.php');
 
 /**
  * Setup $loader object from function intervention
