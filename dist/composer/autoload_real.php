20ef9fc54e68cc0b8f0cba8c3eb7f14ae9f25f82
Darren Jacoby <darren@jacoby.co.za>
2017-08-26 14:21:12 +0200

    Update dist

--- dist/composer/autoload_real.php
+++ dist/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit19a590ab84635cab601c3b0b531b11f2
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit19a590ab84635cab601c3b0b531b11f2', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
