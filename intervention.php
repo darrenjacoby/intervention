a86f2f5789a9eb69ed7b9893d7973f788556dd59
Darren Jacoby <darren@jacoby.dev>
2020-10-26 10:14:03 +0200

    2.0.0

--- intervention.php
+++ intervention.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name:        Intervention
 Plugin URI:         http://github.com/soberwp/intervention
-Description:        WordPress plugin containing modules to cleanup and customize wp-admin
+Description:        Easily customize wp-admin and configure application options.
 Version:            2.0.0-rc.1
 Author:             Sober
 Author URI:         http://github.com/soberwp/
