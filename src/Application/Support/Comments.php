dd3f1275a18048160fb91e123c3856f0e642e0e5
Darren Jacoby <darren@jacoby.dev>
2021-03-30 09:37:35 +0200

    Update Comments.php

--- src/Application/Support/Comments.php
+++ src/Application/Support/Comments.php
@@ -51,6 +51,7 @@ class Comments
     public function removePostTypesSupport()
     {
         $post_types = get_post_types();
+
         foreach ( $post_types as $post_type ) {
             if (post_type_supports($post_type, 'comments') ) {
                 remove_post_type_support($post_type, 'comments');
