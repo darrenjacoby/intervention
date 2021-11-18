83494e99e86524a46eaf2856fec28785f63427e1
Huub <50170696+huubl@users.noreply.github.com>
2021-11-18 17:01:36 +0100

    Fix: join() passing glue string after array is deprecated
    ERROR: join(): Passing glue string after array is deprecated. Swap the parameters

--- src/Support/Arr.php
+++ src/Support/Arr.php
@@ -264,7 +264,7 @@ class Arr
              */
             if (count($key_arr) > 1) {
                 $key_first = array_shift($key_arr);
-                $key_next = join($key_arr, '.');
+                $key_next = implode('.', $key_arr);
                 $carry[$key_first][$key_next] = $v;
             } else {
                 $carry[$k] = $v;
